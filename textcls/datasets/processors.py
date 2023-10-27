0cfd9fd90a54950fa784358e1c38e4ffcf1120ff
htrbao <trbaohoo@gmail.com>
2023-10-28 00:35:32 +0700

    push

--- textcls/datasets/processors.py
+++ textcls/datasets/processors.py
@@ -142,7 +142,7 @@ class IseDsc01Processor(DataProcessor):
             return "2"
     @classmethod
     def get_label_texts(cls):
-        return ["hỗ_trợ ,", "phản_đối ,", "phân_vân ,"]
+        return ["SUPPORTED ,", "REFUTED ,", "NEI ,"]
 
     def _create_examples(self, objs, set_type):
         """Creates examples for the training, dev and test sets."""
